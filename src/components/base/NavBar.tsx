556bcec790a84b2085c4f1598b6148d30fb42f64
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-07 16:20:35 -0800

    feat: change navbar avatar styling

--- src/components/base/NavBar.tsx
+++ src/components/base/NavBar.tsx
@@ -4,10 +4,10 @@ import PersonIcon from "@mui/icons-material/Person";
 import Brand from "../../assets/brand.svg";
 
 const NavBar: React.FC = () => {
-  const [value, setValue] = useState("home");
+  const [tab, setTab] = useState("home");
   const isAuthenticated = true;
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
+    setTab(newValue);
   };
 
   return (
@@ -15,11 +15,11 @@ const NavBar: React.FC = () => {
       component="header"
       position="sticky"
       sx={{
-        px: "30px",
-        py: "15px",
+        px: "20px",
+        py: "10px",
         height: "94",
         borderRadius: "0px 0px 20px 20px",
-        border: "4px solid #3E2B24",
+        border: "6px solid #3E2B24",
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
@@ -28,7 +28,7 @@ const NavBar: React.FC = () => {
     >
       <img src={Brand} alt="Journaler branding" />
       <Tabs
-        value={value}
+        value={tab}
         onChange={handleChange}
         textColor="inherit"
         indicatorColor="secondary"
@@ -45,29 +45,55 @@ const NavBar: React.FC = () => {
         <Tab value="contact" label="Contact" />
         {isAuthenticated ? (
           <Box sx={{ px: "16px", margin: "auto" }}>
-            <IconButton aria-label="user avatar" sx={{ color: "common.white" }}>
-              <Avatar
-                component="image"
-                className="user-icon"
-                sx={[
-                  {
+            {tab === "profile" ? (
+              <IconButton
+                onClick={() => setTab("profile")}
+                aria-label="user avatar"
+                sx={{
+                  color: "common.white",
+                  transition: "0.16s ease-in",
+                  "outline-offset": "-4px",
+                  outline: "2px solid #ff5722",
+                }}
+              >
+                <Avatar
+                  component="image"
+                  className="user-icon"
+                  sx={{
                     bgcolor: "primary.main",
                     color: "common.white",
                     fontWeight: "500",
                     height: "46px",
                     width: "46px",
-                    transition: "0.15s",
-                  },
-                  {
-                    "&:hover": {
-                      "box-shadow": "0px 0px 0px 2px #ff5722",
-                    },
-                  },
-                ]}
+                  }}
+                >
+                  WT
+                </Avatar>
+              </IconButton>
+            ) : (
+              <IconButton
+                onClick={() => setTab("profile")}
+                aria-label="user avatar"
+                sx={{
+                  color: "common.white",
+                  transition: "0.16s ease-in",
+                }}
               >
-                WT
-              </Avatar>
-            </IconButton>
+                <Avatar
+                  component="image"
+                  className="user-icon"
+                  sx={{
+                    bgcolor: "primary.main",
+                    color: "common.white",
+                    fontWeight: "500",
+                    height: "46px",
+                    width: "46px",
+                  }}
+                >
+                  WT
+                </Avatar>
+              </IconButton>
+            )}
           </Box>
         ) : (
           <Tab
