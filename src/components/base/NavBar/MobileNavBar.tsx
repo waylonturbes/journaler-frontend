8c78a79edd4f21e8ab13cf7a17d36de926c546a5
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-08 20:24:58 -0800

    feat: create a mobile navbar

---
+++ src/components/base/NavBar/MobileNavBar.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import AvatarButton from "../AvatarButton";
+import {
+  AppBar,
+  Typography,
+  Tabs,
+  Tab,
+  Box,
+  Toolbar,
+  IconButton,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import PersonIcon from "@mui/icons-material/Person";
+
+const MobileNavBar = () => {
+  const [tab, setTab] = useState("home");
+  const isAuthenticated = true;
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setTab(newValue);
+  };
+
+  const dummyUser = {
+    email: "jkbowser@att.net",
+  };
+  
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "primary.dark" }}>
+      <Toolbar
+        sx={{
+          py: "20px",
+          px: "10px",
+          display: "flex",
+          justifyContent: "space-between",
+          aligntItems: "center",
+        }}
+      >
+        <Box
+          component="div"
+          sx={{
+            display: "flex",
+          }}
+        >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ m: "0px" }}
+          >
+            <MenuIcon fontSize="large" />
+          </IconButton>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: "flex",
+              alignItems: "center",
+              paddingLeft: "10px",
+            }}
+          >
+            Journaler
+          </Typography>
+        </Box>
+        <Button
+          variant="outlined"
+          color="secondary"
+          sx={{
+            py: "12.75px",
+            px: "22px",
+          }}
+        >
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default MobileNavBar;
