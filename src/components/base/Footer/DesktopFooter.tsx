ce666cdb08e09d7b31caeaa292434d4136d77645
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-10 07:39:48 -0800

    feat: create desktop footer component

---
+++ src/components/base/Footer/DesktopFooter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DesktopFooter = () => {
+  return (
+    <>
+      <h1>Hello Work!</h1>
+    </>
+  );
+};
+
+export default DesktopFooter;
---
+++ src/components/base/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DesktopFooter from "./DesktopFooter";
+
+const Footer = () => {
+  return (
+    <>
+      <DesktopFooter />
+    </>
+  );
+};
+
+export default Footer;
