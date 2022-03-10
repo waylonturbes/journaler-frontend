33e34465a317c7b2622410f794d91b4142915b2e
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-10 13:09:31 -0800

    feat: complete desktop footer container

--- src/components/base/Footer/DesktopFooter.tsx
+++ src/components/base/Footer/DesktopFooter.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Box } from "@mui/material";
 
 const DesktopFooter = () => {
   return (
-    <>
-      <h1>Hello Work!</h1>
-    </>
+    <Box
+      component="footer"
+      sx={{
+        p: "30px",
+        backgroundColor: "primary.main",
+        border: "6px solid",
+        borderColor: "primary.dark",
+        borderRadius: "20px 20px 0px 0px",
+      }}
+    ></Box>
   );
 };
 
