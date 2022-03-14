c3525ec2d267bd58d36f9d5f9337ab3352a1ad3f
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-14 08:42:00 -0700

    feat: convert desktop nav tabs to button group

--- src/components/base/NavBar/index.tsx
+++ src/components/base/NavBar/index.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import DesktopNavBar from "./DesktopNavBar";
 import MobileNavBar from "./MobileNavBar";
 import useWindowSize from "../../../utils/useWindowSize";
-import usePathname from "../../../utils/usePathname";
 
 const NavBar = () => {
   let windowSize = useWindowSize();
-  let currentPath = usePathname();
   // will be replaced when connected to backend
   const dummyUser = {
     email: "jkbowser@att.net",
@@ -15,12 +13,8 @@ const NavBar = () => {
 
   return (
     <>
-      {windowSize === 1000 ? (
-        currentPath === "profile" || currentPath === "logout" ? (
-          <DesktopNavBar pathname="home" dummyUser={dummyUser} />
-        ) : (
-          <DesktopNavBar pathname={currentPath} dummyUser={dummyUser} />
-        )
+      {windowSize > 1000 ? (
+        <DesktopNavBar dummyUser={dummyUser} />
       ) : (
         <MobileNavBar dummyUser={dummyUser} />
       )}
