7e04baf56343e5e74e0d1a8a9bc95d689cdbe125
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-21 15:58:17 -0700

    chore: move footer & navbar into app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-
+import NavBar from "./components/base/NavBar";
+import Footer from "./components/base/Footer";
 import {
   About,
   Home,
@@ -10,20 +11,36 @@ import {
   Dashboard,
   Contact,
 } from "./components/pages";
+import { useLocation } from "react-router-dom";
 
 const App: React.FC = () => {
+  const location = useLocation();
+  console.log(location);
   return (
-    <Box sx={{ display: "flex", flex: "auto" }}>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-      </Routes>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        maxWidth: "1200px",
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <NavBar />
+      <Box sx={{ display: "flex", flex: "auto" }}>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </Box>
+      <Footer />
     </Box>
   );
 };
